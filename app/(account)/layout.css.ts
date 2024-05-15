531458460428419790a3e8acc488ce2a2df7ac53
hyunwlee <hwlee3928@gmail.com>
2024-05-16 00:10:21 +0900

    fix(css-style): fix responsive in login, sign-up

--- app/(account)/layout.css.ts
+++ app/(account)/layout.css.ts
@@ -1,9 +1,22 @@
 import { style } from "@vanilla-extract/css";
 
+const main = style({
+  margin: '0 auto',
+  maxWidth: '344px',
+  background: 'transparent',
+  display: 'flex',
+  flexFlow: 'nowrap column',
+  alignItems: 'center',
+
+});
+
 const logo = style({
   margin: '2.667rem 0 3.244rem',
 });
 
-const styles = { logo };
+const styles = {
+  main,
+  logo
+};
 
 export default styles;
--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -1,12 +1,11 @@
 import Logo from "@/app/ui/common/logo";
-import Container from "@/app/ui/common/container";
 import styles from "@/app/(account)/layout.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container>
+    <main className={styles.main}>
       <Logo className={styles.logo} />
       {children}
-    </Container>
+    </main>
   );
 }
